38bb51185f17ffa7397815d41bd36fe5177c7b95
unknown <sanjeetkumarit@outlook.com>
2025-01-20 11:20:04 +0530

    bundled out from production build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  configureWebpack: {
+    externals: {
+      '@achrinza/node-ipc': 'commonjs @achrinza/node-ipc'
+    }
+  },
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
   ? '/my-vue-app/' // Replace 'REPO_NAME' with your repository name
