d7c0e43ad7b4d53c222117a2a1c088349f75116a
unknown <sanjeetkumarit@outlook.com>
2025-01-26 20:40:07 +0530

    vue performance tweaks for parallel build and cache

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require('@vue/cli-service');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-
+const isAnalyzeMode = process.env.ANALYZE === 'true';
 module.exports = defineConfig({
+  parallel: true,
+  chainWebpack: (config) => {
+    config.module
+      .rule('vue')
+      .use('cache-loader')
+      .loader('cache-loader')
+      .end();
+  },
   configureWebpack: {
-    plugins: [new BundleAnalyzerPlugin()],
+    plugins: isAnalyzeMode ? [new BundleAnalyzerPlugin()] : [],
   },
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
